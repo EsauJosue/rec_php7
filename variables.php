d69c0935feaab933e3a862d75c1ffa2a8c542484
Josue Martínez <josue.mart@jmcsoluciones.com>
2020-06-10 20:12:40 -0500

    Variables en php

---
+++ variables.php
@@ -0,0 +1,26 @@
+<?
+//Cadena de Texto
+$nombre = 'Josue';
+
+//Integer : Numeros enteros
+$numero = 7;
+//Numeros con decimales
+$numero_decimal = 7.7;
+//Boolean: Verdadero o falso(true / false)
+$verdadero = false;
+
+#Array: Arreglo
+#Object: Objeto
+#Class: Clase
+#Null: Cuando a una variable aun no se le ha asignado algún valor
+
+//Comentarios con comillas dobles permiten trabajar con variables adentro
+
+echo "Hola, $nombre";
+
+echo 'Hola ' . $nombre;
+
+//Mostrar tipo de variable
+echo gettype($numero_decimal);
+
+?>
